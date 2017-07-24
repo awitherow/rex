db048fc681958f6c14e904f1259792ee86b14d68
awitherow <au.witherow@gmail.com>
2017-07-24 23:54:27 +0200

    feat(run): add prompts

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,26 @@ cd "$parent_path"
 
 source ../.env
 
+if [ "$REX_USER_NAME" == "" ]
+then
+    echo "Welcome!"
+    echo "What is your name? "
+    read REX_USER_NAME
+    echo "REX_USER_NAME=$REX_USER_NAME" >> ../.env
+fi
+
+echo "Welcome, $REX_USER_NAME!"
+
+echo "Which coin are you trading from? "
+
+read COIN_FORM
+
+export "$COIN_FROM"
+
+echo "Which coin do you wish to trade to? "
+
+read COIN_TO
+
+export "$COIN_TO" 
+
 rex
